b0aadbf2a48e202ba4055e3d2dca7ca9bbad3873
CrioUser <crio-user@criodo.com>
2025-04-04 01:18:54 +0530

    update

--- resources/js/index.js
+++ resources/js/index.js
@@ -31,10 +31,11 @@ const carousels = ["carousel1Items", "carousel2Items", "carousel3Items"];
     });
 //close the first accordian when another button is clicked
 document.querySelectorAll("button[data-bs-toggle='collapse']").forEach(button=>{
-  button.addEventListener("click",()=>{
+  button.addEventListener("click",(event)=>{
+    const targetAccordion = document.querySelector(button.getAttribute("data-bs-target"));
     const firstAccordion = document.getElementById("accordion1");
-    if(firstAccordion && firstAccordion.classList.contains("show")){
-      new bootstrap.Collapse(firstAccordion,{toggle:true});
+    if(firstAccordion && firstAccordion !==targetAccordion && firstAccordion.classList.contains("show")){
+      bootstrap.Collapse (firstAccordion,{toggle:false}).hide();
     }
   });
 });
