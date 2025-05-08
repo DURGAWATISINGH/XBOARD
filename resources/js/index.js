87583586af442d6d5b560af0147783639f534ad1
CrioUser <crio-user@criodo.com>
2025-05-08 21:34:20 +0530

    yes

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,4 +1,4 @@
-  const magazines = [
+ /* const magazines = [
   "https://flipboard.com/@thenewsdesk/the-latest-on-coronavirus-covid-19-t82no8kmz.rss",
   "https://flipboard.com/@dfletcher/india-tech-b2meqpd6z.rss",
   "https://flipboard.com/@thehindu/sportstarlive-rj3ttinvz.rss"
@@ -40,4 +40,50 @@ document.querySelectorAll("button[data-bs-toggle='collapse']").forEach(button=>{
      firstAccordion.hide();
     }
   });
+});*/
+const magazines = [
+  "https://flipboard.com/@thenewsdesk/the-latest-on-coronavirus-covid-19-t82no8kmz.rss",
+  "https://flipboard.com/@dfletcher/india-tech-b2meqpd6z.rss",
+  "https://flipboard.com/@thehindu/sportstarlive-rj3ttinvz.rss"
+];
+
+const carousels = ["carousel1Items", "carousel2Items", "carousel3Items"];
+
+magazines.forEach((url, index) => {
+  fetch(`https://api.rss2json.com/v1/api.json?rss_url=${url}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const items = data.items;
+      const carousel = document.getElementById(carousels[index]);
+
+      items.forEach((item, i) => {
+        const div = document.createElement("div");
+        div.className = `carousel-item ${i === 0 ? "active" : ""}`;
+        div.innerHTML = `
+          <div class="card h-100" onclick="window.open('${item.link}', '_blank')" style="cursor: pointer;">
+            <img src="${item.enclosure?.link || 'https://via.placeholder.com/150'}" class="card-img-top" alt="News Image">
+            <div class="card-body">
+              <h5 class="card-title">${item.title}</h5>
+              <p class="card-text">${item.description}</p>
+            </div>
+          </div>
+        `;
+        carousel.appendChild(div);
+      });
+    });
 });
+
+// Accordion logic to collapse first one when others open
+document.querySelectorAll("button[data-bs-toggle='collapse']").forEach(button => {
+  button.addEventListener("click", () => {
+    const targetId = button.getAttribute("data-bs-target");
+    const targetAccordion = document.querySelector(targetId);
+    const firstAccordion = document.getElementById("accordion1");
+
+    if (firstAccordion && firstAccordion !== targetAccordion && firstAccordion.classList.contains("show")) {
+      const instance = bootstrap.Collapse.getInstance(firstAccordion) || new bootstrap.Collapse(firstAccordion, { toggle: false });
+      instance.hide();
+    }
+  });
+});
+
